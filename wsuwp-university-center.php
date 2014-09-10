038fc704b9e87388a5678463f3daffdc239901a5
jeremyfelt <jeremy.felt@gmail.com>
2014-09-10 10:34:10 -0700

    Add initial files

---
+++ wsuwp-university-center.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: University Center
+Plugin URI: http://web.wsu.edu/wordpress/plugins/university-center/
+Description: Provide custom content types and taxonomies for a university center or organization.
+Author: washingtonstateuniversity, jeremyfelt
+Version: 0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+class WSUWP_University_Center {
+	public function __construct() {
+
+	}
+}
+new WSUWP_University_Center();
