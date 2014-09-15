a4b01f87bbc1ad9665bc13fb50c6e373172f348b
jeremyfelt <jeremy.felt@gmail.com>
2014-09-15 13:39:05 -0700

    Bump version to 0.1.1

--- wsuwp-university-center.php
+++ wsuwp-university-center.php
@@ -4,7 +4,7 @@ Plugin Name: University Center
 Plugin URI: http://web.wsu.edu/wordpress/plugins/university-center/
 Description: Provide custom content types and taxonomies for a university center or organization.
 Author: washingtonstateuniversity, jeremyfelt
-Version: 0.1.0
+Version: 0.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
