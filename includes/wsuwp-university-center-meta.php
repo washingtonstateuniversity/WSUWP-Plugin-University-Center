95aba6b84cf71a3adf3984254ec7ddbb3f38acfd
jeremyfelt <jeremy.felt@gmail.com>
2015-01-16 16:23:25 -0800

    Convert object URL to work alongside other meta
    We don't need this hanging out as its own meta box when the
    information is being captured alongside everything else.

--- includes/wsuwp-university-center-meta.php
+++ includes/wsuwp-university-center-meta.php
@@ -21,8 +21,6 @@ class WSUWP_University_Center_Meta {
 			return;
 		}
 
-		add_meta_box( 'wsuwp_uc_object_url', 'URL', array( $this, 'display_object_url_meta_box' ), null, 'normal', 'default' );
-
 		if ( $post_type === wsuwp_uc_get_object_type_slug( 'people' ) ) {
 			add_meta_box( 'wsuwp_uc_person_info', 'Information', array( $this, 'display_person_information_meta_box' ), null, 'normal', 'default' );
 		}
@@ -37,7 +35,7 @@ class WSUWP_University_Center_Meta {
 	 *
 	 * @param WP_Post $post
 	 */
-	public function display_object_url_meta_box( $post ) {
+	private function display_object_url_meta_box( $post ) {
 		$object_url = get_post_meta( $post->ID, '_wsuwp_uc_object_url', true );
 
 		if ( ! empty( $object_url ) ) {
@@ -46,7 +44,6 @@ class WSUWP_University_Center_Meta {
 			$object_url = '';
 		}
 
-		wp_nonce_field( 'save_object_url', '_uc_object_url_nonce' );
 		?>
 		<label for="wsuwp-uc-object-url">URL:</label>
 		<input type="text" class="widefat" id="wsuwp-uc-object-url" name="wsuwp_uc_object_url" value="<?php echo $object_url; ?>" />
@@ -70,6 +67,9 @@ class WSUWP_University_Center_Meta {
 			<div class="project-information-id">
 				<label for="wsuwp-uc-project-id">Project ID:</label>
 				<input type="text" id="wsuwp-uc-project-id" name="wsuwp_uc_project_id" value="<?php echo esc_attr( $project_id ); ?>" />
+			</div>
+			<div class="project-information-url">
+				<?php $this->display_object_url_meta_box( $post ); ?>
 			</div>
 			<div class="clear"></div>
 		</div>
@@ -134,6 +134,8 @@ class WSUWP_University_Center_Meta {
 
 				<label for="wsuwp-uc-person-phone">Phone Number:</label>
 				<input type="text" id="wsuwp-uc-person-phone" name="wsuwp_uc_person_phone" value="<?php echo esc_attr( $person_phone ); ?>" />
+
+				<?php $this->display_object_url_meta_box( $post ); ?>
 			</div>
 			<div class="clear"></div>
 		</div>
@@ -144,26 +146,8 @@ class WSUWP_University_Center_Meta {
 	 * Assign a URL to an object when saved through the object's meta box.
 	 *
 	 * @param int     $post_id The ID of the post being saved.
-	 * @param WP_Post $post    The full post object being saved.
 	 */
-	public function save_object_url( $post_id, $post ) {
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-			return;
-		}
-
-		// Only assign a unique id to content from our registered types.
-		if ( ! in_array( $post->post_type, wsuwp_uc_get_object_type_slugs() ) ) {
-			return;
-		}
-
-		if ( 'auto-draft' === $post->post_status ) {
-			return;
-		}
-
-		if ( ! isset( $_POST['_uc_object_url_nonce'] ) || false === wp_verify_nonce( $_POST['_uc_object_url_nonce'], 'save_object_url' ) ) {
-			return;
-		}
-
+	private function save_object_url( $post_id ) {
 		if ( isset( $_POST['wsuwp_uc_object_url'] ) ) {
 			if ( empty( trim( $_POST['wsuwp_uc_object_url'] ) ) ) {
 				delete_post_meta( $post_id, '_wsuwp_uc_object_url' );
@@ -205,6 +189,8 @@ class WSUWP_University_Center_Meta {
 				update_post_meta( $post_id, '_wsuwp_uc_project_id', sanitize_text_field( $_POST['wsuwp_uc_project_id'] ) );
 			}
 		}
+
+		$this->save_object_url( $post_id );
 
 		return;
 	}
@@ -303,6 +289,8 @@ class WSUWP_University_Center_Meta {
 				update_post_meta( $post_id, '_wsuwp_uc_person_phone', sanitize_text_field( $_POST['wsuwp_uc_person_phone'] ) );
 			}
 		}
+
+		$this->save_object_url( $post_id );
 
 		return;
 	}
