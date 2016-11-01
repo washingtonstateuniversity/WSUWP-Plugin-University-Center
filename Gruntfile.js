e9370b6ff66e7697531dca31820a6e568da9c643
Jeremy Felt <jeremy.felt@gmail.com>
2016-11-01 12:54:39 -0700

    Adjust JavaScript to meet code standards

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function( grunt ) {
                 options: {
                     preset: "jquery",
                     requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
-                    maximumLineLength: 250
+                    maximumLineLength: 550
                 }
             }
         },
